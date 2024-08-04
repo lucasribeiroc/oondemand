ae0355d2e42fe9d04c08bdb87ec835e99c41b5ec
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-04 17:11:26 -0300

    chore: Update global CSS and Tailwind configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#001E27]">
       <header className="bg-[#00343F] w-full h-[79px] flex justify-center items-center">
-        <Image src="/logo.png" alt="Logo" width={200} height={200} />
+        <a href="#">
+          <Image src="/logo.png" alt="Logo" width={200} height={200} />
+        </a>
       </header>
-      <div>
+      <div className="relative h-[756px]">
         <Carousel
           showThumbs={false}
           showStatus={false}
@@ -68,7 +70,7 @@ export default function Home() {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
-                  viewBox="0 24 24"
+                  viewBox="0 0 24 24"
                   strokeWidth={2}
                   stroke="#2E363E"
                   className="w-6 h-6"
@@ -82,17 +84,16 @@ export default function Home() {
               </button>
             )
           }
-          className="h-[1440px]"
+          className="relative h-[756px]"
         >
-          <div className="relative">
+          <div className="relative h-[756px]">
             <Image
               src="/carrossel1.png"
               alt="Carrossel 1"
-              layout="responsive"
-              width={1440}
-              height={1440}
+              layout="fill"
+              objectFit="cover"
             />
-            <div className="absolute top-1/2 left-44 transform -translate-y-1/2 text-white text-[48px] font-extrabold">
+            <div className="absolute top-1/2 left-4 md:left-44 transform -translate-y-1/2 text-white text-[24px] md:text-[48px] font-extrabold">
               <div className="text-left mb-[1px]">
                 Teste Sob
                 <br className="mb-[0.5px]" />
@@ -101,27 +102,25 @@ export default function Home() {
                 <span className="font-normal">do pedido ao</span>
                 <br className="mb-[0.5px]" />
                 pagamento
-                <br className="mb-[30px]" /> {/* Alterado de 40px para 30px */}
-                <div className="font-normal text-[24px] leading-none">
+                <br className="mb-[30px]" />
+                <div className="font-normal text-[16px] md:text-[24px] leading-none">
                   Seu desejo é uma ordem! Você só
                   <div className="mt-[20px]">paga pelo tempo otimizado!</div>
-                  <button className="mt-[60px] w-[292px] h-[48px] bg-[#00E2F4] border border-[#6EDFF6] text-[#001E27] text-[20px] font-bold rounded-[10px] hover:bg-[#00c2d4] active:transform active:scale-95 transition-transform">
+                  <button className="mt-[20px] md:mt-[60px] w-[200px] md:w-[292px] h-[40px] md:h-[48px] bg-[#00E2F4] border border-[#6EDFF6] text-[#001E27] text-[16px] md:text-[20px] font-bold rounded-[10px] hover:bg-[#00c2d4] active:transform active:scale-95 transition-transform">
                     Conheça nossos truques
-                  </button>{" "}
-                  {/* Alterado de 40px para 60px */}
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <div className="relative">
+          <div className="relative h-[756px]">
             <Image
-              src="/carrossel1.png"
-              alt="Carrossel 1"
-              layout="responsive"
-              width={1440}
-              height={1440}
+              src="/carrossel2.png"
+              alt="Carrossel 2"
+              layout="fill"
+              objectFit="cover"
             />
-            <div className="absolute top-1/2 left-44 transform -translate-y-1/2 text-white text-[48px] font-extrabold">
+            <div className="absolute top-1/2 left-4 md:left-44 transform -translate-y-1/2 text-white text-[24px] md:text-[48px] font-extrabold">
               <div className="text-left mb-[1px]">
                 Teste Sob
                 <br className="mb-[0.5px]" />
@@ -130,27 +129,25 @@ export default function Home() {
                 <span className="font-normal">do pedido ao</span>
                 <br className="mb-[0.5px]" />
                 pagamento
-                <br className="mb-[30px]" /> {/* Alterado de 40px para 30px */}
-                <div className="font-normal text-[24px] leading-none">
+                <br className="mb-[30px]" />
+                <div className="font-normal text-[16px] md:text-[24px] leading-none">
                   Seu desejo é uma ordem! Você só
                   <div className="mt-[20px]">paga pelo tempo otimizado!</div>
-                  <button className="mt-[60px] w-[292px] h-[48px] bg-[#00E2F4] border border-[#6EDFF6] text-[#001E27] text-[20px] font-bold rounded-[10px] hover:bg-[#00c2d4] active:transform active:scale-95 transition-transform">
+                  <button className="mt-[20px] md:mt-[60px] w-[200px] md:w-[292px] h-[40px] md:h-[48px] bg-[#00E2F4] border border-[#6EDFF6] text-[#001E27] text-[16px] md:text-[20px] font-bold rounded-[10px] hover:bg-[#00c2d4] active:transform active:scale-95 transition-transform">
                     Conheça nossos truques
-                  </button>{" "}
-                  {/* Alterado de 40px para 60px */}
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <div className="relative">
+          <div className="relative h-[756px]">
             <Image
-              src="/carrossel1.png"
-              alt="Carrossel 1"
-              layout="responsive"
-              width={1440}
-              height={1440}
+              src="/carrossel3.png"
+              alt="Carrossel 3"
+              layout="fill"
+              objectFit="cover"
             />
-            <div className="absolute top-1/2 left-44 transform -translate-y-1/2 text-white text-[48px] font-extrabold">
+            <div className="absolute top-1/2 left-4 md:left-44 transform -translate-y-1/2 text-white text-[24px] md:text-[48px] font-extrabold">
               <div className="text-left mb-[1px]">
                 Teste Sob
                 <br className="mb-[0.5px]" />
@@ -159,14 +156,13 @@ export default function Home() {
                 <span className="font-normal">do pedido ao</span>
                 <br className="mb-[0.5px]" />
                 pagamento
-                <br className="mb-[30px]" /> {/* Alterado de 40px para 30px */}
-                <div className="font-normal text-[24px] leading-none">
+                <br className="mb-[30px]" />
+                <div className="font-normal text-[16px] md:text-[24px] leading-none">
                   Seu desejo é uma ordem! Você só
                   <div className="mt-[20px]">paga pelo tempo otimizado!</div>
-                  <button className="mt-[60px] w-[292px] h-[48px] bg-[#00E2F4] border border-[#6EDFF6] text-[#001E27] text-[20px] font-bold rounded-[10px] hover:bg-[#00c2d4] active:transform active:scale-95 transition-transform">
+                  <button className="mt-[20px] md:mt-[60px] w-[200px] md:w-[292px] h-[40px] md:h-[48px] bg-[#00E2F4] border border-[#6EDFF6] text-[#001E27] text-[16px] md:text-[20px] font-bold rounded-[10px] hover:bg-[#00c2d4] active:transform active:scale-95 transition-transform">
                     Conheça nossos truques
-                  </button>{" "}
-                  {/* Alterado de 40px para 60px */}
+                  </button>
                 </div>
               </div>
             </div>
