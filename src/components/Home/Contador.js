132f75ae7529f80076935253a16b316c97cbef51
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-07 20:22:25 -0300

    refactor: Update Contador component styles
    This commit updates the styles of the Contador component to improve the layout and enhance the overall user experience. The inline styles are replaced with Tailwind CSS utility classes to achieve a more concise and maintainable code. These changes make the component more visually appealing and user-friendly.

--- src/components/Home/Contador.js
+++ src/components/Home/Contador.js
@@ -2,32 +2,32 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const TimeUnit = ({ label, value }) => (
-  <div className="flex flex-col items-center">
-    <div className="text-center">{label}</div>
-    <div className="text-[59.42px] font-poppins font-normal text-white">
+  <div className="flex flex-col items-center text-center md:text-left">
+    <div>{label}</div>
+    <div className="text-[40px] md:text-[59.42px] font-poppins font-normal text-white">
       {value}
     </div>
   </div>
 );
 
 const TimerDisplay = ({ time }) => (
-  <div className="flex flex-col items-center justify-center ml-auto text-white text-[11.55px] font-light font-poppins pr-5 md:mr-12">
+  <div className="flex flex-col items-center justify-center mt-5 md:mt-0 ml-4 md:ml-auto text-white text-[11.55px] font-light font-poppins pr-5 md:mr-12 text-center md:text-left">
     <div className="flex space-x-4">
       <TimeUnit label="Dias" value={time.days} />
       <div className="flex items-center">
-        <div className="text-[59.42px] font-poppins font-normal text-white mt-[10px]">
+        <div className="text-[40px] md:text-[59.42px] font-poppins font-normal text-white mt-[10px]">
           :
         </div>
       </div>
       <TimeUnit label="Horas" value={time.hours} />
       <div className="flex items-center">
-        <div className="text-[59.42px] font-poppins font-normal text-white mt-[10px]">
+        <div className="text-[40px] md:text-[59.42px] font-poppins font-normal text-white mt-[10px]">
           :
         </div>
       </div>
       <TimeUnit label="Minutos" value={time.minutes} />
       <div className="flex items-center">
-        <div className="text-[59.42px] font-poppins font-normal text-white mt-[10px]">
+        <div className="text-[40px] md:text-[59.42px] font-poppins font-normal text-white mt-[10px]">
           :
         </div>
       </div>
@@ -89,14 +89,14 @@ const Contador = () => {
 
   return (
     <div
-      className="flex flex-col md:flex-row items-center justify-center md:justify-start p-8 h-[calc(100vh-100px)] md:h-auto"
+      className="flex flex-col md:flex-row items-center justify-center md:justify-start p-8 h-[calc(100vh-100px)] md:h-auto pt-16 pb-16 md:pt-8 md:pb-8"
       style={{ backgroundColor: "#001E27" }}
     >
       <div className="flex flex-col md:flex-row items-center w-full">
         <div className="hidden md:block">
           <Image src="/cartola.png" alt="Cartola" width={362} height={362} />
         </div>
-        <div className="ml-0 md:ml-[55px] pl-4 text-[#00E2F4] font-bold text-[36px] uppercase leading-none tracking-[0.105em] text-center md:text-left whitespace-nowrap">
+        <div className="ml-4 md:ml-[55px] pl-0 md:pl-4 text-[#00E2F4] font-bold text-[36px] uppercase leading-none tracking-[0.105em] text-center md:text-left whitespace-nowrap">
           <div>Num Passe</div>
           <div>De Mágica</div>
         </div>
