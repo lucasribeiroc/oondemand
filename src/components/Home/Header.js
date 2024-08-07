76f52cb4bebfa60404decee29bd0f3e3990be846
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-07 15:17:03 -0300

    refactor: Update logo in Header component
    This commit updates the logo in the Header component. The previous logo image "/logo.png" is replaced with "/logo-header.svg" to improve the visual appeal and branding of the website. The new logo image is imported and rendered in the Header component. This change enhances the overall user experience and promotes our brand identity.

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-[#00343F] w-full h-[79px] flex justify-center items-center">
       <a href="#">
-        <Image src="/logo.png" alt="Logo" width={200} height={200} />
+        <Image src="/logo-header.svg" alt="Logo" width={200} height={200} />
       </a>
     </header>
   );
