cf7cff541ad704c3a1478061ee796fddb5d01f7a
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-07 16:01:45 -0300

    refactor: Update Footer component with logo and navigation links
    This commit updates the Footer component to include a logo and navigation links. The logo is added using the "logo-footer.svg" image, enhancing the visual appeal and branding of the website. The navigation links are organized into three sections: "Discovery", "About", and "Info", providing easy access to relevant information for the users. This change improves the overall layout and user experience of the website.

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,14 +1,58 @@
 const Footer = () => {
   return (
     <div className="w-full h-[460px]">
-      <div className="w-full h-[383px] bg-[#001E27]">
-        {/* Conteúdo da primeira div */}
+      <div className="w-full h-[383px] bg-[#001E27] pl-24 flex items-center">
+        <img
+          src="logo-footer.svg"
+          alt="Logo Footer"
+          className="w-[200px] h-[200px]"
+        />
+        <div className="flex">
+          <div className="pl-[350px]">
+            <p className="text-[#00E2F4] font-montserrat text-[16px]">
+              Discovery
+            </p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              New Season
+            </p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              Most searched
+            </p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              Most selled
+            </p>
+          </div>
+          <div className="pl-[120px]">
+            <p className="text-[#00E2F4] font-montserrat text-[16px]">About</p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              Help
+            </p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              Shipping
+            </p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              Affiliate
+            </p>
+          </div>
+          <div className="pl-[120px]">
+            <p className="text-[#00E2F4] font-montserrat text-[16px]">Info</p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              Contact us
+            </p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              Privacy Policies
+            </p>
+            <p className="text-[#E1E1E1] font-montserrat text-[16px] mt-8">
+              Terms & Conditions
+            </p>
+          </div>
+        </div>
       </div>
       <div className="w-full h-[76px] bg-[#e5e5e5] flex items-center justify-between">
-        <p className="text-[#5E6E89] pl-20">
+        <p className="text-[#5E6E89] pl-8">
           ©oOndemand todos direitos reservados
         </p>
-        <p className="text-[#5E6E89] pr-20">Designed with ❤️ by Ancor midia</p>
+        <p className="text-[#5E6E89] pr-8">Designed with ❤️ by Ancor midia</p>
       </div>
     </div>
   );
