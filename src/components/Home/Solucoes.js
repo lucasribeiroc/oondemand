5d8dbc1f500a1c45837f61966fcbff040a85bf6d
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-07 18:58:05 -0300

    refactor: Update Solucoes component styles
    This commit updates the styles of the Solucoes component to improve the layout and enhance the overall user experience. The inline styles are replaced with Tailwind CSS utility classes to achieve a more concise and maintainable code. These changes make the component more visually appealing and user-friendly.

--- src/components/Home/Solucoes.js
+++ src/components/Home/Solucoes.js
@@ -2,26 +2,19 @@ import Image from "next/image";
 
 const Solucoes = () => {
   return (
-    <div style={{ backgroundColor: "#001E27" }}>
+    <div className="bg-[#001E27]">
       <div className="flex items-center justify-center mt-[-80px]">
-        {/* Ajuste a margem superior */}
-        <div
-          className="text-[#00E2F4] font-poppins text-[48px] font-bold mb-[60px]"
-          style={{ letterSpacing: "-0.9px" }}
-        >
+        <div className="text-[#00E2F4] font-poppins text-[48px] font-bold mb-[60px] tracking-[-0.9px]">
           O que você precisa?
         </div>
       </div>
       <div className="flex items-center justify-center">
-        <div
-          className="font-poppins text-[24px] font-medium text-white"
-          style={{ letterSpacing: "0.1px" }}
-        >
+        <div className="font-poppins text-[24px] font-medium text-white tracking-[0.1px]">
           Resolvemos seus problemas em um passe de mágica
         </div>
       </div>
 
-      {/* Nova seção com 3 cards */}
+      {/* Cards */}
       <div className="flex justify-center mt-20 space-x-[120px] items-center">
         {/* Primeiro card */}
         <div className="w-[272.12px] h-[186.49px] bg-[#00353F] rounded-[7.28px] flex flex-col items-center justify-center p-[10px]">
