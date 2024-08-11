68db9100486f40312f95d177a27b50babdeaa703
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-10 23:30:15 -0300

    refactor: Update Clientes component styles
    This commit updates the styles of the Clientes component to improve the layout and enhance the overall user experience. The inline styles are modified to adjust the padding and image sizes, making the component more visually appealing and responsive.

--- src/components/Home/Clientes.js
+++ src/components/Home/Clientes.js
@@ -20,25 +20,25 @@ const Clientes = () => {
         Nossos <span style={{ fontWeight: "bold" }}>Clientes</span>
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center w-full">
-        <div className="text-center w-full">
+        <div className="text-center w-full pt-10 md:pt-0">
           <img
             src="/clientes1.png"
             alt="Cliente 1"
             className="block w-full max-w-xs mx-auto" // Ajusta o tamanho da imagem conforme a tela
           />
         </div>
-        <div className="text-center w-full">
+        <div className="text-center w-full pt-10 md:pt-0">
           <img
             src="/clientes2.png"
             alt="Cliente 2"
             className="block w-full max-w-xs mx-auto" // Ajusta o tamanho da imagem conforme a tela
           />
         </div>
-        <div className="text-center w-full md:col-span-2 lg:col-span-1 md:pb-12 lg:pb-0">
+        <div className="text-center w-full md:col-span-2 lg:col-span-1 md:pb-12 lg:pb-0 pt-10 md:pt-0">
           <img
             src="/clientes3.png"
             alt="Cliente 3"
-            className="block w-full max-w-xs mx-auto" // Ajusta o tamanho da imagem conforme a tela
+            className="block mx-auto h-[79px]" // Centraliza a imagem e define o tamanho
           />
         </div>
       </div>
