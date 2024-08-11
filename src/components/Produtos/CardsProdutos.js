f529a53388b1aa95e00e633df8646a7ff3d7fffd
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-10 21:11:39 -0300

    refactor: Update CardsProdutos component styles
    This commit updates the styles of the CardsProdutos component to improve the layout and enhance the overall user experience. The component now uses the CardPageProdutos component for rendering product cards, providing an improved design and layout. These changes make the component more visually appealing and user-friendly.

--- src/components/Produtos/CardsProdutos.js
+++ src/components/Produtos/CardsProdutos.js
@@ -98,17 +98,17 @@ const CardsProdutos = () => {
     : listaModulos;
 
   return (
-    <section className="w-full bg-[#001E27] flex items-center justify-center">
-      <div className="w-[1071px] h-[1551px] bg-[#001E27] mx-auto pt-[50px]">
-        <h2 className="text-[#00E2F4] font-montserrat font-bold text-[18px] tracking-[0.2px]">
+    <section className="w-full bg-[#001E27] flex items-center justify-center py-8 md:py-12 lg:py-20">
+      <div className="w-full lg:w-[1071px] h-auto lg:h-[1551px] bg-[#001E27] mx-auto pt-[20px]">
+        <h2 className="text-[#00E2F4] font-montserrat font-bold text-[18px] tracking-[0.2px] text-center lg:text-left">
           Categorias
         </h2>
-        <div className="flex space-x-4 mt-4">
+        <div className="flex flex-wrap justify-center lg:justify-start space-x-4 mt-4">
           {filters.map((filter) => (
             <button
               key={filter}
               onClick={() => toggleFilter(filter)}
-              className={`font-montserrat font-medium text-[14px] tracking-[0.2px] px-4 py-1 ${
+              className={`font-montserrat font-medium text-[12px] md:text-[14px] tracking-[0.2px] px-4 py-1 mb-2 ${
                 selectedFilters.includes(filter)
                   ? "bg-[#00E2F4] text-[#001E27] rounded-[30px]"
                   : "text-white"
@@ -119,7 +119,7 @@ const CardsProdutos = () => {
           ))}
         </div>
         <hr className="w-full border-t border-white mt-4" />
-        <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center lg:justify-items-start">
           {filteredModulos.map((modulo, index) => (
             <CardPageProdutos key={index} modulo={modulo} />
           ))}
