fe77d34f0273a25ecc3b9b12b71699924794a88b
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-12 20:22:21 -0300

    refactor: Update Header component to use Next.js Image component for logo

--- src/components/Produtos/Header.js
+++ src/components/Produtos/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <header className="bg-[#00343F] w-full h-[79px] flex justify-center items-center">
-      <a href="#">
+      <a href="/">
         <Image src="/logo-header.svg" alt="Logo" width={200} height={200} />
       </a>
     </header>
