2fa1e26329b3c53859f587b7761687f44d1dc2ad
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-12 20:32:51 -0300

    refactor: Update Header and Produtos/Header components to use Next.js Link component for logo
    This commit updates the Header and Produtos/Header components to use the Next.js Link component instead of the <a> tag for the logo. The Link component provides better performance and optimization for navigation within the application. Additionally, the logo is wrapped in a Link component with the "/" path specified as the href attribute. This change improves the overall efficiency and user experience of the components.

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="bg-[#00343F] w-full h-[79px] flex justify-center items-center">
-      <a href="/">
+      <Link href="/">
         <Image src="/logo-header.svg" alt="Logo" width={200} height={200} />
-      </a>
+      </Link>
     </header>
   );
 };
