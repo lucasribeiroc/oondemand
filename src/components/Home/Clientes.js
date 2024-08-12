498c17354f5373abcb10821f4a91f956e2ecbe32
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-12 20:12:25 -0300

    refactor: Update Clientes component to use Next.js Image component for displaying client images
    This commit updates the Clientes component to use the Next.js Image component instead of the <img> tag for displaying the client images. The Image component provides better performance and optimization for loading and displaying images. Additionally, the image sources are updated to use absolute paths ("/clientes1.png", "/clientes2.png", "/clientes3.png") and the width and height attributes are specified to ensure proper rendering. This change improves the overall efficiency and user experience of the component.

--- src/components/Home/Clientes.js
+++ src/components/Home/Clientes.js
@@ -3,42 +3,46 @@ import Image from "next/image";
 const Clientes = () => {
   return (
     <div className="w-full bg-white p-5 pb-24 box-border">
-      {" "}
-      {/* Ajusta o padding-bottom para 100px */}
       <h1
         style={{
           fontFamily: "Montserrat, sans-serif",
           fontSize: "48px",
           letterSpacing: "-1px",
           color: "#00353F",
-          textAlign: "center", // Centraliza o texto
-          margin: "0", // Remove margem padrão do h1
-          paddingTop: "50px", // Adiciona padding-top de 50px
-          paddingBottom: "50px", // Adiciona padding-bottom de 50px
+          textAlign: "center",
+          margin: "0",
+          paddingTop: "50px",
+          paddingBottom: "50px",
         }}
       >
         Nossos <span style={{ fontWeight: "bold" }}>Clientes</span>
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center w-full">
         <div className="text-center w-full pt-10 md:pt-0">
-          <img
+          <Image
             src="/clientes1.png"
             alt="Cliente 1"
-            className="block w-full max-w-xs mx-auto" // Ajusta o tamanho da imagem conforme a tela
+            className="block w-full max-w-xs mx-auto"
+            width={500}
+            height={500}
           />
         </div>
         <div className="text-center w-full pt-10 md:pt-0">
-          <img
+          <Image
             src="/clientes2.png"
             alt="Cliente 2"
-            className="block w-full max-w-xs mx-auto" // Ajusta o tamanho da imagem conforme a tela
+            className="block w-full max-w-xs mx-auto"
+            width={500}
+            height={500}
           />
         </div>
         <div className="text-center w-full md:col-span-2 lg:col-span-1 md:pb-12 lg:pb-0 pt-10 md:pt-0">
-          <img
+          <Image
             src="/clientes3.png"
             alt="Cliente 3"
-            className="block mx-auto h-[79px]" // Centraliza a imagem e define o tamanho
+            className="block mx-auto h-[79px] w-auto"
+            width={500} // Valor padrão para evitar erro
+            height={79}
           />
         </div>
       </div>
