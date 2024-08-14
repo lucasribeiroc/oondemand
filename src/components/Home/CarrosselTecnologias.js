36dffb7576bcd38ecd4eaea4cb766961c85cb036
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-14 18:09:17 -0300

    ```text refactor: Update CarrosselTecnologias component to use flexbox instead of react-slick
    This commit updates the CarrosselTecnologias component to replace the react-slick library with a flexbox implementation. The Slider component, NextArrow component, and PrevArrow component are removed, and the images are displayed using flexbox and Image components from the next/image package. This change simplifies the code and improves performance by reducing dependencies.

--- src/components/Home/CarrosselTecnologias.js
+++ src/components/Home/CarrosselTecnologias.js
@@ -1,17 +1,4 @@
 import Image from "next/image";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-const NextArrow = (props) => {
-  const { className, onClick } = props;
-  return <div className={`${className} block bg-none`} onClick={onClick} />;
-};
-
-const PrevArrow = (props) => {
-  const { className, onClick } = props;
-  return <div className={`${className} block bg-none`} onClick={onClick} />;
-};
 
 const CarrosselTecnologias = () => {
   return (
@@ -37,100 +24,61 @@ const CarrosselTecnologias = () => {
       </div>
 
       <div className="flex justify-center mt-10 md:mt-20 pb-20">
-        <div className="text-center w-[90%] md:w-[90%] h-auto">
-          <Slider
-            infinite={true}
-            slidesToShow={3}
-            slidesToScroll={1}
-            autoplay={true}
-            autoplaySpeed={2000}
-            nextArrow={<NextArrow />}
-            prevArrow={<PrevArrow />}
-            centerMode={true}
-            centerPadding="20px"
-            responsive={[
-              {
-                breakpoint: 1930,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 1,
-                },
-              },
-              {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 3,
-                },
-              },
-              {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 2,
-                },
-              },
-              {
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 1,
-                },
-              },
-            ]}
-          >
-            <div className="px-5">
-              <Image
-                src="/partner1.png"
-                alt="Partner 1"
-                className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
-                width={150}
-                height={100}
-              />
-            </div>
-            <div className="px-0">
-              <Image
-                src="/partner2.png"
-                alt="Partner 2"
-                className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
-                width={150}
-                height={100}
-              />
-            </div>
-            <div className="px-0">
-              <Image
-                src="/partner3.png"
-                alt="Partner 3"
-                className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
-                width={150}
-                height={100}
-              />
-            </div>
-            <div className="px-0">
-              <Image
-                src="/partner4.png"
-                alt="Partner 4"
-                className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
-                width={150}
-                height={100}
-              />
-            </div>
-            <div className="px-0">
-              <Image
-                src="/partner5.png"
-                alt="Partner 5"
-                className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
-                width={150}
-                height={100}
-              />
-            </div>
-            <div className="px-0">
-              <Image
-                src="/partner6.png"
-                alt="Partner 6"
-                className="h-auto mx-5 max-h-[100px] md:max-h-[150px] w-auto"
-                width={150}
-                height={100}
-              />
-            </div>
-          </Slider>
+        <div className="text-center w-[90%] md:w-[90%] h-auto flex flex-wrap justify-center">
+          <div className="px-5 mb-8 md:mb-0">
+            <Image
+              src="/partner1.png"
+              alt="Partner 1"
+              className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
+              width={150}
+              height={100}
+            />
+          </div>
+          <div className="px-5 mb-8 md:mb-0">
+            <Image
+              src="/partner2.png"
+              alt="Partner 2"
+              className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
+              width={150}
+              height={100}
+            />
+          </div>
+          <div className="px-5 mb-8 md:mb-0">
+            <Image
+              src="/partner3.png"
+              alt="Partner 3"
+              className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
+              width={150}
+              height={100}
+            />
+          </div>
+          <div className="px-5 mb-8 md:mb-0">
+            <Image
+              src="/partner4.png"
+              alt="Partner 4"
+              className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
+              width={150}
+              height={100}
+            />
+          </div>
+          <div className="px-5 mb-8 md:mb-0">
+            <Image
+              src="/partner5.png"
+              alt="Partner 5"
+              className="h-auto mx-5 max-h-[100px] md:max-h-[150px]"
+              width={150}
+              height={100}
+            />
+          </div>
+          <div className="px-5 mb-8 md:mb-0">
+            <Image
+              src="/partner6.png"
+              alt="Partner 6"
+              className="h-auto mx-5 max-h-[100px] md:max-h-[150px] w-auto"
+              width={150}
+              height={100}
+            />
+          </div>
         </div>
       </div>
     </div>
